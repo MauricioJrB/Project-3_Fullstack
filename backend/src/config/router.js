ac87bf01fe4c4fe2183830b4a24541c621c261ac
MauricioJrB <mjunior1205@gmail.com>
2024-08-19 23:15:20 -0300

    feat: added Anime router protected

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import userRoutes from '../routes/Users.js';
+import userRoutes from '../routes/users.js';
+import animeRoutes from '../routes/Animes.js';
 
 const routerConfig = app => {
   const protectedRouter = express.Router();
 
   protectedRouter.use('/users', userRoutes);
+  protectedRouter.use('/animes', animeRoutes);
 
   app.use('/api', protectedRouter);
 };
