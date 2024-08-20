2ce8a7ab61441d0b5ac06e9a24a7a9fc4986ab59
MauricioJrB <mjunior1205@gmail.com>
2024-08-20 17:00:42 -0300

    feat: adedd app middleware erros into app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import db from './config/dbConnect.js';
 import routerConfig from './config/router.js';
-import middlewares from './config/middlewares.js';
 
 const app = express();
 
 app.db = db;
-middlewares(app);
+app.use(bodyParser.json());
+
 routerConfig(app);
 
 app.get('/', (req, res) => {
   res.status(200).send({ msg: 'Welcome API Animes' });
+});
+
+app.use((err, req, res, next) => {
+  const { name, message, stack } = err;
+  if (name === 'AuthError') res.status(403).json({ error: message });
+  else if (name === 'UserNotFound' || name === 'AnimeNotFound') res.status(404).json({ error: message });
+  else if (name === 'ValidationError') res.status(422).json({ error: message });
+  else res.status(500).json({ name, message, stack });
+  next(err);
 });
 
 export default app;
