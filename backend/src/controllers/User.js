07a605a2e49233dcb245f823f1ba0de2cadbd23a
MauricioJrB <mjunior1205@gmail.com>
2024-08-21 00:36:58 -0300

    feat: added sign in controller and password hash function

--- backend/src/controllers/User.js
+++ backend/src/controllers/User.js
@@ -1,8 +1,32 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jwt-simple';
 import UserNotFound from '../errors/UserNotFound.js';
 import ValidationError from '../errors/ValidationError.js';
 import UserServices from '../services/User.js';
 
+const { SECRET } = process.env;
+
+const getPasswordHash = password => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+};
 class UserController {
+  static signIn = async (req, res, next) => {
+    await UserServices.getByEmail({ email: req.body.email })
+      .then(user => {
+        if (!user) return next(new ValidationError('Invalid User or Password'));
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          const payload = {
+            id: user.id,
+            email: user.email,
+          };
+          const token = jwt.encode(payload, SECRET);
+          return res.status(200).json({ token });
+        } return next(new ValidationError('Invalid User or Password'));
+      })
+      .catch(err => next(err));
+  };
+
   static create = async (req, res, next) => {
     const { email, password } = req.body;
 
@@ -14,22 +38,23 @@ class UserController {
     const userExists = await UserServices.getByEmail({ email });
     if (userExists) return next(new ValidationError('There is already a user with this email'));
 
-    const newUser = { email, password };
+    const passwordEncrypted = getPasswordHash(password);
+
+    const newUser = { email, password: passwordEncrypted };
 
     return UserServices.create(newUser)
       .then(user => res.status(201).json({ User: user }))
       .catch(err => next(err));
   };
 
-  static getUserById = async (req, res, next) => {
-    const userId = req.params.id;
-
-    return UserServices.getUserById(userId)
-      .then(user => {
-        if (!user) return next(new UserNotFound('User not found'));
-        return res.status(200).json({ user });
-      })
-      .catch(err => next(err));
+  static getUserById = async userId => {
+    try {
+      const user = UserServices.getUserById(userId);
+      if (!user) throw new UserNotFound('User not found');
+      return user;
+    } catch (err) {
+      return err;
+    }
   };
 
   static getAllUsers = async (req, res, next) => {
