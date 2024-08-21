d2e59e3e0cde9285dbf62c16661b32650f3fcf6a
MauricioJrB <mjunior1205@gmail.com>
2024-08-21 18:00:19 -0300

    feat: added middleware validate on router auth

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import UserController from '../controllers/User.js';
+import validate from '../config/middlewares.js';
 
 const router = express.Router();
-router.post('/signin', UserController.signIn);
-router.post('/signup', UserController.create);
+router.post('/signin', validate('createUser'), UserController.signIn);
+router.post('/signup', validate('createUser'), UserController.createUser);
 
 export default router;
