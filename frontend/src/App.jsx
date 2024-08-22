891328d2ba5929cabf1c7fd2a442932b914bb0aa
MauricioJrB <mjunior1205@gmail.com>
2024-08-22 20:57:25 -0300

    feat: added private route and added Home page on app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
-import Animes from "./components/Animes";
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import SignIn from "./pages/SignIn/components/SignIn";
 import SignUp from "./pages/SignUp/components/SignUp";
+import PrivateRoute from "./utils/ProtectedRoutes";
+import { AuthProvider } from "./contexts/UserContext";
+import Home from "./pages/Home/Home";
 
 const App = () => {
    return (
    <>
-      <Routes>
+     <AuthProvider>
+       <Routes>
          <Route path="/" element={<SignIn />} />
          <Route path="/signup" element={<SignUp />} />
-         <Route path="/animes" element={<Animes />} />
+         <Route element={<PrivateRoute />}>
+            <Route path="/animes" element={<Home />} />
+         </Route>
       </Routes>
+     </AuthProvider>
    </>
    );
-}
+};
 
 export default App;
