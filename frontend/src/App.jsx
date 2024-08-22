81bf46f40dbac5b5a6ee921f7757566e338e6fe7
MauricioJrB <mjunior1205@gmail.com>
2024-08-21 21:20:22 -0300

    feat: defined routes on App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import Animes from "./components/Animes";
+import { Routes, Route} from 'react-router-dom'
+import SignIn from "./pages/SignIn/components/SignIn";
+import SignUp from "./pages/SignUp/components/SignUp";
 
 const App = () => {
    return (
-      <>
-         <Animes />
-      </>
+   <>
+      <Routes>
+         <Route path="/" element={<SignIn />} />
+         <Route path="/signup" element={<SignUp />} />
+         <Route path="/animes" element={<Animes />} />
+      </Routes>
+   </>
    );
 }
 
