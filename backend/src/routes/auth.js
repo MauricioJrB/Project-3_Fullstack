41fb0c636ce8c908c83e7ccebec15654a592128d
MauricioJrB <mjunior1205@gmail.com>
2024-09-09 15:08:27 -0300

    feat: updated auth router

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import UserController from '../controllers/User.js';
-import validate from '../config/middlewares.js';
+import validate from '../middlewares/sanitzers.js';
 
 const router = express.Router();
 router.post('/signin', validate('createUser'), UserController.signIn);
-router.post('/signup', validate('createUser'), UserController.createUser);
+router.post('/signup', validate('createUser'), UserController.signUp);
 
 export default router;
