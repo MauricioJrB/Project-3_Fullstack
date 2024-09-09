5c7a5f82f085579a98e4adf505ef00de9de5ea03
MauricioJrB <mjunior1205@gmail.com>
2024-09-09 15:10:12 -0300

    feat: added rateLimiter ans cache middlewares in protectedRouter

--- backend/src/config/router.js
+++ backend/src/config/router.js
@@ -2,14 +2,16 @@ import express from 'express';
 import userRoutes from '../routes/users.js';
 import animeRoutes from '../routes/animes.js';
 import authRoutes from '../routes/auth.js';
-import passportConfig from './passport.js';
+import passportConfig from '../middlewares/passport.js';
+import cache from '../middlewares/cache.js';
+import { rateLimiter } from '../middlewares/rateLimiter.js';
 
 const routerConfig = app => {
   app.use('/auth', authRoutes);
   const protectedRouter = express.Router();
 
-  protectedRouter.use('/users', userRoutes);
-  protectedRouter.use('/animes', animeRoutes);
+  protectedRouter.use('/users', rateLimiter, cache, userRoutes);
+  protectedRouter.use('/animes', rateLimiter, cache, animeRoutes);
 
   app.use('/api', passportConfig(), protectedRouter);
 };
