1a500df1ade242ce1792619f633df074acf6a9fd
MauricioJrB <mjunior1205@gmail.com>
2024-09-09 15:11:43 -0300

    feat: added helmet, morgan, compreesion, logger and cors in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,18 +2,40 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import compression from 'compression';
 import db from './config/dbConnect.js';
 import routerConfig from './config/router.js';
+import logger from './config/logger.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(compression());
 app.db = db;
 app.use(bodyParser.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: 'https://localhost:3000',
+  methods: 'GET, POST',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use((req, res, next) => {
+  logger.info(`Request ${req.method} ${req.url}`);
+  next();
+});
+
 routerConfig(app);
 
 app.get('/', (req, res) => {
   res.status(200).send({ msg: 'Welcome API Animes' });
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Encoding', 'gzip');
+  next();
 });
 
 app.use((err, req, res, next) => {
